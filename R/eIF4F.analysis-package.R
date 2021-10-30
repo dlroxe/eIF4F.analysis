bdabe018913934dfaafd65bea852b1b8720d02b4
Su Wu <wusu2014@gmail.com>
2021-10-30 15:39:49 -0400

    chang the namespace file to import dependent libraries

---
+++ R/eIF4F.analysis-package.R
@@ -0,0 +1,48 @@
+#' @import AnnotationDbi
+#' @import BiocGenerics
+#' @import circlize
+#' @import clusterProfiler
+#' @import ComplexHeatmap
+#' @import corrplot
+#' @import data.table
+#' @import dplyr
+#' @import EnvStats
+#' @import eulerr
+#' @import factoextra
+#' @import FactoMineR
+#' @import forcats
+#' @import forestplot
+#' @import ggfortify
+#' @import ggpubr
+#' @import ggplot2
+#' @import graphics
+#' @import grDevices
+#' @import grid
+#' @import lattice
+#' @import magrittr
+#' @import limma
+#' @import missMDA
+#' @import org.Hs.eg.db
+#' @import purrr
+#' @import RColorBrewer
+#' @import ReactomePA
+#' @import readr
+#' @import readxl
+#' @import reshape2
+#' @import scales
+#' @import stats
+#' @import stats4
+#' @import stringr
+#' @import survival
+#' @import survivalAnalysis
+#' @import tibble
+#' @import tidyr
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+
+
