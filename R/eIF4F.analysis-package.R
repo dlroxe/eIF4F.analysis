96f022be7976df2f37d5083b241c60d2e712006c
Su Wu <wusu2014@gmail.com>
2021-11-12 17:41:08 -0500

    update the license file and namespace

--- R/eIF4F.analysis-package.R
+++ R/eIF4F.analysis-package.R
@@ -1,33 +1,24 @@
 #' @import AnnotationDbi
-#' @import BiocGenerics
 #' @import circlize
 #' @import clusterProfiler
 #' @import ComplexHeatmap
-#' @import corrplot
 #' @import data.table
 #' @import dplyr
-#' @import EnvStats
 #' @import eulerr
-#' @import factoextra
-#' @import FactoMineR
 #' @import forcats
 #' @import ggfortify
 #' @import ggpubr
 #' @import ggplot2
 #' @import graphics
-#' @import grDevices
 #' @import grid
 #' @import lattice
 #' @import magrittr
 #' @import limma
-#' @import missMDA
 #' @import org.Hs.eg.db
 #' @import purrr
 #' @import RColorBrewer
 #' @import ReactomePA
-#' @import readr
 #' @import readxl
-#' @import reshape2
 #' @import scales
 #' @import stats
 #' @import stats4
@@ -36,7 +27,6 @@
 #' @import tidyr
 #' @importFrom dplyr distinct filter select select_if mutate mutate_at mutate_if
 #' summarise rename group_by
-#' @importFrom forcats fct_rev
 #' @importFrom rlang .data %||% set_names sym expr enexpr quo enquo parse_expr
 #' @importFrom reshape2 melt dcast
 #' @keywords internal
