a9653f4d145c4ced7543786df2ec85686cace124
Su Wu <wusu2014@gmail.com>
2022-10-04 16:23:01 -0400

    file existence check before download, and replace xlsx files with gz format

--- Script/Download.R
+++ Script/Download.R
@@ -16,120 +16,158 @@ options(timeout = 200000)
 
 ### TCGA and GTEX DATA
 #### TCGA CNV dataset (thresholded) 1
-utils::download.file("https://tcga.xenahubs.net/download/TCGA.PANCAN.sampleMap/Gistic2_CopyNumber_Gistic2_all_thresholded.by_genes.gz",
-  file.path(data_file_directory, "Gistic2_CopyNumber_Gistic2_all_thresholded.by_genes.gz"),
-  method = "libcurl"
-)
+if (!file.exists(file.path(data_file_directory, "Gistic2_CopyNumber_Gistic2_all_thresholded.by_genes"))) {
+  utils::download.file("https://tcga.xenahubs.net/download/TCGA.PANCAN.sampleMap/Gistic2_CopyNumber_Gistic2_all_thresholded.by_genes.gz",
+    destfile = file.path(data_file_directory, "Gistic2_CopyNumber_Gistic2_all_thresholded.by_genes.gz"),
+    method = "libcurl"
+  )
+  R.utils::gunzip(file.path(data_file_directory, "Gistic2_CopyNumber_Gistic2_all_thresholded.by_genes.gz"))
+}
 
 
 #### TCGA CNV dataset 2
-utils::download.file("https://tcga.xenahubs.net/download/TCGA.PANCAN.sampleMap/Gistic2_CopyNumber_Gistic2_all_data_by_genes.gz",
-  file.path(data_file_directory, "Gistic2_CopyNumber_Gistic2_all_data_by_genes.gz"),
-  method = "libcurl"
-)
+if (!file.exists(file.path(data_file_directory, "Gistic2_CopyNumber_Gistic2_all_data_by_genes"))) {
+  utils::download.file("https://tcga.xenahubs.net/download/TCGA.PANCAN.sampleMap/Gistic2_CopyNumber_Gistic2_all_data_by_genes.gz",
+    destfile = file.path(data_file_directory, "Gistic2_CopyNumber_Gistic2_all_data_by_genes.gz"),
+    method = "libcurl"
+  )
+  R.utils::gunzip(file.path(data_file_directory, "Gistic2_CopyNumber_Gistic2_all_data_by_genes.gz"))
+}
+
 
 #### TCGA CNV ratio dataset 3
-utils::download.file("https://pancanatlas.xenahubs.net/download/broad.mit.edu_PANCAN_Genome_Wide_SNP_6_whitelisted.gene.xena.gz",
-  file.path(data_file_directory, "broad.mit.edu_PANCAN_Genome_Wide_SNP_6_whitelisted.gene.xena.gz"),
-  method = "libcurl"
-)
+if (!file.exists(file.path(data_file_directory, "broad.mit.edu_PANCAN_Genome_Wide_SNP_6_whitelisted.gene.xena"))) {
+  utils::download.file("https://pancanatlas.xenahubs.net/download/broad.mit.edu_PANCAN_Genome_Wide_SNP_6_whitelisted.gene.xena.gz",
+                       file.path(data_file_directory, "broad.mit.edu_PANCAN_Genome_Wide_SNP_6_whitelisted.gene.xena.gz"),
+                       method = "libcurl"
+  )
+  R.utils::gunzip(file.path(data_file_directory, "broad.mit.edu_PANCAN_Genome_Wide_SNP_6_whitelisted.gene.xena.gz"))
+}
 
 
 #### TCGA RNA-Seq dataset 4
-utils::download.file("https://tcga-pancan-atlas-hub.s3.us-east-1.amazonaws.com/download/EB%2B%2BAdjustPANCAN_IlluminaHiSeq_RNASeqV2.geneExp.xena.gz",
-  file.path(data_file_directory, "EB++AdjustPANCAN_IlluminaHiSeq_RNASeqV2.geneExp.xena.gz"),
-  method = "libcurl"
-)
+if (!file.exists(file.path(data_file_directory, "EB++AdjustPANCAN_IlluminaHiSeq_RNASeqV2.geneExp.xena"))) {
+  utils::download.file("https://tcga-pancan-atlas-hub.s3.us-east-1.amazonaws.com/download/EB%2B%2BAdjustPANCAN_IlluminaHiSeq_RNASeqV2.geneExp.xena.gz",
+                       file.path(data_file_directory, "EB++AdjustPANCAN_IlluminaHiSeq_RNASeqV2.geneExp.xena.gz"),
+                       method = "libcurl"
+  )
+  R.utils::gunzip(file.path(data_file_directory, "EB++AdjustPANCAN_IlluminaHiSeq_RNASeqV2.geneExp.xena.gz"))
+}
+
 
 #### TCGA sample type annotation 5
-utils::download.file("https://pancanatlas.xenahubs.net/download/TCGA_phenotype_denseDataOnlyDownload.tsv.gz",
-  file.path(data_file_directory, "TCGA_phenotype_denseDataOnlyDownload.tsv.gz"),
-  method = "libcurl"
-)
+if (!file.exists(file.path(data_file_directory, "TCGA_phenotype_denseDataOnlyDownload.tsv"))) {
+  utils::download.file("https://pancanatlas.xenahubs.net/download/TCGA_phenotype_denseDataOnlyDownload.tsv.gz",
+                       file.path(data_file_directory, "TCGA_phenotype_denseDataOnlyDownload.tsv.gz"),
+                       method = "libcurl"
+  )
+  R.utils::gunzip(file.path(data_file_directory, "TCGA_phenotype_denseDataOnlyDownload.tsv.gz"))
+}
+
 
 #### TCGA OS data ## 6
-utils::download.file("https://tcga-pancan-atlas-hub.s3.us-east-1.amazonaws.com/download/Survival_SupplementalTable_S1_20171025_xena_sp",
-  file.path(data_file_directory, "Survival_SupplementalTable_S1_20171025_xena_sp"),
-  method = "libcurl"
-)
+if (!file.exists(file.path(data_file_directory, "Survival_SupplementalTable_S1_20171025_xena_sp"))) {
+  utils::download.file("https://tcga-pancan-atlas-hub.s3.us-east-1.amazonaws.com/download/Survival_SupplementalTable_S1_20171025_xena_sp",
+                       file.path(data_file_directory, "Survival_SupplementalTable_S1_20171025_xena_sp"),
+                       method = "libcurl"
+  )
+}
 
 #### TCGA and GTEX RNA-Seq dataset 7
-utils::download.file("https://toil.xenahubs.net/download/TcgaTargetGtex_RSEM_Hugo_norm_count.gz",
-  file.path(data_file_directory, "TcgaTargetGtex_RSEM_Hugo_norm_count.gz"),
-  method = "libcurl"
-)
+if (!file.exists(file.path(data_file_directory, "TcgaTargetGtex_RSEM_Hugo_norm_count"))) {
+  utils::download.file("https://toil.xenahubs.net/download/TcgaTargetGtex_RSEM_Hugo_norm_count.gz",
+                       file.path(data_file_directory, "TcgaTargetGtex_RSEM_Hugo_norm_count.gz"),
+                       method = "libcurl"
+  )
+  R.utils::gunzip(file.path(data_file_directory, "TcgaTargetGtex_RSEM_Hugo_norm_count.gz"))
+}
+
 
 #### TCGA and GTEX sample type annotation 8
-utils::download.file("https://toil.xenahubs.net/download/TcgaTargetGTEX_phenotype.txt.gz",
-  file.path(data_file_directory, "TcgaTargetGTEX_phenotype.txt.gz"),
-  method = "libcurl"
-)
-
+if (!file.exists(file.path(data_file_directory, "TcgaTargetGTEX_phenotype.txt"))) {
+  utils::download.file("https://toil.xenahubs.net/download/TcgaTargetGTEX_phenotype.txt.gz",
+                       file.path(data_file_directory, "TcgaTargetGTEX_phenotype.txt.gz"),
+                       method = "libcurl"
+  )
+  R.utils::gunzip(file.path(data_file_directory, "TcgaTargetGTEX_phenotype.txt.gz"))
+}
 
 ### CPTAC DATA
 
 #### CPTAC LUAD RNA-Seq data (Gillette et al., 2020) 9
-utils::download.file("https://github.com/a3609640/EIF-analysis/raw/master/LUAD%20Data/RNA.xlsx.gz",
-  file.path(data_file_directory, "RNA.xlsx"),
-  method = "libcurl"
-)
+if (!file.exists(file.path(data_file_directory, "RNA.xlsx"))) {
+  utils::download.file("https://github.com/a3609640/EIF-analysis/raw/master/LUAD%20Data/RNA.xlsx.gz",
+                       file.path(data_file_directory, "RNA.xlsx.gz"),
+                       method = "libcurl"
+  )
+  R.utils::gunzip(file.path(data_file_directory, "RNA.xlsx.gz"))
+}
 
 #### CPTAC LUAD Proteomics (Gillette et al., 2020) 10
-utils::download.file("https://github.com/a3609640/EIF-analysis/raw/master/LUAD%20Data/Protein.xlsx.gz",
-  file.path(data_file_directory, "Protein.xlsx"),
-  method = "libcurl"
-)
+if (!file.exists(file.path(data_file_directory, "Protein.xlsx"))) {
+  utils::download.file("https://github.com/a3609640/EIF-analysis/raw/master/LUAD%20Data/Protein.xlsx.gz",
+                       file.path(data_file_directory, "Protein.xlsx.gz"),
+                       method = "libcurl"
+  )
+  R.utils::gunzip(file.path(data_file_directory, "Protein.xlsx.gz"))
+}
 
 #### CPTAC LUAD Phosproteomics (Gillette et al., 2020) 11
-utils::download.file("https://github.com/a3609640/EIF-analysis/raw/master/LUAD%20Data/Phos.xlsx.gz",
-  file.path(data_file_directory, "Phos.xlsx"),
-  method = "libcurl"
-)
+if (!file.exists(file.path(data_file_directory, "Phos.xlsx"))) {
+  utils::download.file("https://github.com/a3609640/EIF-analysis/raw/master/LUAD%20Data/Phos.xlsx.gz",
+                       file.path(data_file_directory, "Phos.xlsx.gz"),
+                       method = "libcurl"
+  )
+  R.utils::gunzip(file.path(data_file_directory, "Phos.xlsx.gz"))
+}
 
 #### CPTAC LUAD Sample Annotation 12
 # S046_BI_CPTAC3_LUAD_Discovery_Cohort_Samples_r1_May2019.xlsx from PDC Study ID: PDC000153
-utils::download.file("https://github.com/a3609640/EIF-analysis/raw/master/LUAD%20Data/S046_BI_CPTAC3_LUAD_Discovery_Cohort_Samples_r1_May2019.xlsx",
-  file.path(data_file_directory, "S046_BI_CPTAC3_LUAD_Discovery_Cohort_Samples_r1_May2019.xlsx.gz"),
-  method = "libcurl"
-)
+if (!file.exists(file.path(data_file_directory, "S046_BI_CPTAC3_LUAD_Discovery_Cohort_Samples_r1_May2019.xlsx"))) {
+  utils::download.file("https://github.com/a3609640/EIF-analysis/raw/master/LUAD%20Data/S046_BI_CPTAC3_LUAD_Discovery_Cohort_Samples_r1_May2019.xlsx.gz",
+                       file.path(data_file_directory, "S046_BI_CPTAC3_LUAD_Discovery_Cohort_Samples_r1_May2019.xlsx.gz"),
+                       method = "libcurl"
+  )
+  R.utils::gunzip(file.path(data_file_directory, "S046_BI_CPTAC3_LUAD_Discovery_Cohort_Samples_r1_May2019.xlsx.gz"))
+}
 
 #### CPTAC LUAD Clinical Data 13
 # S046_BI_CPTAC3_LUAD_Discovery_Cohort_Clinical_Data_r1_May2019.xlsx from PDC Study ID: PDC000153
-utils::download.file("https://github.com/a3609640/EIF-analysis/raw/master/LUAD%20Data/S046_BI_CPTAC3_LUAD_Discovery_Cohort_Clinical_Data_r1_May2019.xlsx",
-  file.path(data_file_directory, "S046_BI_CPTAC3_LUAD_Discovery_Cohort_Clinical_Data_r1_May2019.xlsx.gz"),
-  method = "libcurl"
-)
-
+if (!file.exists(file.path(data_file_directory, "S046_BI_CPTAC3_LUAD_Discovery_Cohort_Clinical_Data_r1_May2019.xlsx"))) {
+  utils::download.file("https://github.com/a3609640/EIF-analysis/raw/master/LUAD%20Data/S046_BI_CPTAC3_LUAD_Discovery_Cohort_Clinical_Data_r1_May2019.xlsx.gz",
+                       file.path(data_file_directory, "S046_BI_CPTAC3_LUAD_Discovery_Cohort_Clinical_Data_r1_May2019.xlsx.gz"),
+                       method = "libcurl"
+  )
+  R.utils::gunzip(file.path(data_file_directory, "S046_BI_CPTAC3_LUAD_Discovery_Cohort_Clinical_Data_r1_May2019.xlsx.gz"))
+}
 
 ### CCLE DATA
 
 #### CCLE RNA-Seq data from DepMap Public 20Q4 20Q3 14
-utils::download.file("https://ndownloader.figshare.com/files/24613349",
-  file.path(data_file_directory, "CCLE_expression_full.csv"),
-  method = "libcurl"
-)
+if (!file.exists(file.path(data_file_directory, "CCLE_expression_full.csv"))) {
+  utils::download.file("https://ndownloader.figshare.com/files/24613349",
+                       file.path(data_file_directory, "CCLE_expression_full.csv"),
+                       method = "libcurl"
+  )
+}
 
 #### CCLE annotation data 15
-utils::download.file("https://ndownloader.figshare.com/files/24613394",
-  file.path(data_file_directory, "sample_info.csv"),
-  method = "libcurl"
-)
-
+if (!file.exists(file.path(data_file_directory, "sample_info.csv"))) {
+  utils::download.file("https://ndownloader.figshare.com/files/24613394",
+                       file.path(data_file_directory, "sample_info.csv"),
+                       method = "libcurl"
+  )
+}
 
 #### CCLE proteomics data 16
-utils::download.file("https://gygi.hms.harvard.edu/data/ccle/protein_quant_current_normalized.csv.gz",
-  file.path(data_file_directory, "protein_quant_current_normalized.csv.gz"),
-  method = "libcurl"
-)
+if (!file.exists(file.path(data_file_directory, "protein_quant_current_normalized.csv"))) {
+  utils::download.file("https://gygi.hms.harvard.edu/data/ccle/protein_quant_current_normalized.csv.gz",
+                       file.path(data_file_directory, "protein_quant_current_normalized.csv.gz"),
+                       method = "libcurl"
+  )
+  R.utils::gunzip(file.path(data_file_directory, "protein_quant_current_normalized.csv.gz"))
+}
 
-# decompress files
-# lapply(list.files(getwd(), pattern = ".gz"), gunzip)
 
-R.utils::gunzip(file.path(data_file_directory,"broad.mit.edu_PANCAN_Genome_Wide_SNP_6_whitelisted.gene.xena.gz"))
-R.utils::gunzip(file.path(data_file_directory,"EB++AdjustPANCAN_IlluminaHiSeq_RNASeqV2.geneExp.xena.gz"))
-R.utils::gunzip(file.path(data_file_directory,"Gistic2_CopyNumber_Gistic2_all_data_by_genes.gz"))
-R.utils::gunzip(file.path(data_file_directory,"Gistic2_CopyNumber_Gistic2_all_thresholded.by_genes.gz"))
-R.utils::gunzip(file.path(data_file_directory,"protein_quant_current_normalized.csv.gz"))
-R.utils::gunzip(file.path(data_file_directory,"TCGA_phenotype_denseDataOnlyDownload.tsv.gz"))
-R.utils::gunzip(file.path(data_file_directory,"TcgaTargetGTEX_phenotype.txt.gz"))
-R.utils::gunzip(file.path(data_file_directory,"TcgaTargetGtex_RSEM_Hugo_norm_count.gz"))
+
+
